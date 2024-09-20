1c9c8925e3484cdb95c70bf05d09c7a287d8bd8e
Diego444666 <fachlevi98@gmail.com>
2024-09-20 18:49:55 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan3.py
@@ -0,0 +1,115 @@
+# print("hallo")
+# cuaca = "cerah"
+
+# if cuaca == "cerah":
+#     print("saya akan pergi ke pantai")
+
+# else:
+#     print("ke rumah teman")
+
+# uang = 1
+
+# if uang:
+#     print("saya bisa pergi ke pantai")
+
+# else:
+#     print("saya tidak bisa pergi ke pantai")
+
+# nilai = 70
+
+# if nilai < 70:
+#     print("nilai kurang")
+
+# else:
+#     print("nilai cukup")
+
+# nilai = 0
+# if nilai < 0:
+#     print("bilangan negatif")
+
+# else:
+#     print("bilangan positif")
+
+# Umur = int(input("Masukkan umur Anda : "))
+
+# if Umur <= 10:
+
+#     print( " anak-anak")
+
+# elif Umur <= 18:
+
+#     print( "remaja")
+
+# elif Umur <=35:
+
+#     print(" dewasa")
+
+# elif Umur <=65:
+
+#     print("paruh baya")
+
+# else:
+
+#     print("Tua")
+
+# nilai = 76
+
+# if nilai >= 80 :
+#     if nilai >= 85 :
+#         print("Sangat baik")
+
+#     else :
+#         print("Baik")
+# else :
+#     print("jelek")
+#salah
+# nilai = int(input("Masukkan nilai Anda : "))
+# if nilai < 2 :
+#         if nilai <= 2 :
+#             print("nonton film")
+
+#         else :
+#             print("coding")
+# else :
+#         print("ngoding")
+
+# print ("menu")
+# print ("1. nonton film")
+# print ("2. ngoding")
+# print ("3. keluar")
+
+# Umur = int(input("Masukkan nomor : "))
+
+# if Umur == 1:
+
+#     print( "nonton film")
+
+# elif Umur == 2:
+
+#     print( "ngoding")
+
+# elif Umur ==3:
+
+#     print(" keluar")
+
+# else:
+
+#     print("menu tidak tersedia")
+
+# angka = 10
+
+# if angka % 2  == 0 :
+#     print("angka genap")
+# if angka > 0:
+#     print("angka positif")
+
+# if angka  % 2 == 0 :
+#     print("angka genap") 
+# elif angka  > 0 :
+#     print("angka positif")
+
+
+
+
+
+
