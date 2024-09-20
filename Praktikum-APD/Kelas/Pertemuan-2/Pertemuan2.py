2ea9dbc48249e8934cd583955044056ae9c2c808
Diego444666 <fachlevi98@gmail.com>
2024-09-20 20:02:38 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan-2/Pertemuan2.py
@@ -0,0 +1,37 @@
+# penulisan pertama snake case
+nama_mahasiswa = "ucup"
+
+# camel case
+
+# namaMahahasiswaInformatika = "ucup2"
+
+# NamaMahasiswa = "ucup3"
+
+# #maha siswa = 22
+
+# #if = 2
+# #list  = 2
+
+# #numerik int
+# #number = 1
+# #numebr2 = -1
+
+# #float
+
+# #number3 = 1.2
+
+# #number4 = -1.5
+
+# # string
+
+# #fakultas = "teknik"
+# #prodi = 'informatika'
+# #umur = "28"
+
+# #kalimat """Aku bisa
+# #banyak
+# #kata"""
+
+# kalimat = "Aku bisa ngoding"
+
+# print (kalimat)
