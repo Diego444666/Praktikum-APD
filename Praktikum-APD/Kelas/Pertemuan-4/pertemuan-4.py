dbdacc68e445e92f69d4d4592aad620ee24232c8
Diego444666 <fachlevi98@gmail.com>
2024-09-27 15:02:27 +0800

    Add files via upload

---
+++ Praktikum-APD/Kelas/Pertemuan-4/pertemuan-4.py
@@ -0,0 +1,74 @@
+# batas = 20
+# for i in range(1,batas,2): 
+#     print(f"perulangan ke-{i}")
+
+# buah =["apel", "mangga", "anggur"]
+
+# for 
+
+# for i in buah:
+#     print(i)
+
+
+# for buah in buah_banyak:
+#     print(buah)
+
+# for baris in range(1,4):
+#     print("baris ke", baris)
+#     for kolom in range(1,4):
+#         print(kolom, "kolom", end=" ")
+
+#     print()
+#     print() 
+
+# while "hello":
+#     print("hello")
+
+# lagi = "yes"
+
+# ulang = 0
+
+# while lagi == "yes":
+#     ulang +=1
+
+#     print("mabar")
+    
+#     lagi = input("apakah kamu ingin mabar lagi? (yes/no): ")
+
+# print("selesai ")
+
+# print(f"perulangan terjadii= sebanyak {ulang} kali")
+
+# for i in range (1,10):
+#     if i == 4
+#     break
+#     else:
+# print(i)
+
+
+# buah =["apel", "mangga", "anggur"]
+
+# for buah in buah_banyak:
+
+#     if buah =="anggur"
+#         print(buah)
+
+
+# while True:
+#     ulang = input("mabar lagio")
+
+#     if ulang == "ga":
+#         break
+#     print("mabar lagi:")
+
+
+# for i in range(1, 10):
+#     if i % 2 == 1:
+#         continue
+# print(i)
+
+for i in range (5):
+    for j in range(5):
+        print("*", end=" ")
+
+    print()
